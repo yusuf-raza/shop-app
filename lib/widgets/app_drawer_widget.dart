7654251feb92e91eae6e47b99b37d9449f8b83f1
Syed Yusuf Raza Zaidi <zaidiam95@gmail.com>
2022-09-25 11:17:54 +0500

    app drawer updated

--- lib/widgets/app_drawer_widget.dart
+++ lib/widgets/app_drawer_widget.dart
@@ -11,43 +11,70 @@ class AppDrawerWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Drawer(
-      child: Column(
-        children: [
-          AppBar(
-            title: const Text('hello!'),
-            automaticallyImplyLeading: false,
-          ),
-          const Divider(),
-          ListTile(
-            leading: const Icon(Icons.shop),
-            title: const Text('shop'),
-            onTap: () => Navigator.of(context).pushReplacementNamed('/'),
-          ),
-          const Divider(),
-          ListTile(
-            leading: const Icon(Icons.payment),
-            title: const Text('orders'),
-            onTap: () => Navigator.of(context)
-                .pushReplacementNamed(OrdersScreen.routeName),
-          ),
-          const Divider(),
-          ListTile(
-            leading: const Icon(Icons.edit),
-            title: const Text('manage products'),
-            onTap: () => Navigator.of(context)
-                .pushReplacementNamed(UserProductScreen.routeName),
-          ),
-          const Divider(),
-          ListTile(
-            leading: const Icon(Icons.logout),
-            title: const Text('logout'),
-            onTap: ()  {
-             // Navigator.of(context).pushReplacementNamed(AuthScreen.routeName);
-              Provider.of<AuthProvider>(context,listen: false).logOut();
-              Navigator.pop(context);
-            },
-          )
-        ],
+      child: Container(
+        color: Colors.deepPurple,
+        child: ListView(
+          children: [
+            const Image(image: AssetImage("assets/images/empty-cart.png")),
+/*
+            DrawerHeader(
+                //curve: Curves.easeIn,
+                padding: EdgeInsets.zero,
+                child: UserAccountsDrawerHeader(
+                    currentAccountPicture: CircleAvatar(
+                      backgroundImage: NetworkImage(
+                          "https://pbs.twimg.com/profile_images/1350086629264863233/ydwSLiHl_400x400.jpg"),
+                    ),
+                    accountName: Text("Yusuf Raza"),
+                    accountEmail: Text("yusuf@gmail.com"))),
+*/
+            ListTile(
+              leading: const Icon(Icons.shop, color: Colors.white),
+              title: const Text('All products',
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                  textScaleFactor: 1.2),
+              onTap: () => Navigator.of(context).pushReplacementNamed('/'),
+            ),
+            const Divider(),
+            ListTile(
+              leading: const Icon(Icons.payment, color: Colors.white),
+              title: const Text('Your orders',
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                  textScaleFactor: 1.2),
+              onTap: () => Navigator.of(context)
+                  .pushReplacementNamed(OrdersScreen.routeName),
+            ),
+            const Divider(),
+            ListTile(
+              leading: const Icon(Icons.edit, color: Colors.white),
+              title: const Text('Manage your products',
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                  textScaleFactor: 1.2),
+              onTap: () => Navigator.of(context)
+                  .pushReplacementNamed(UserProductScreen.routeName),
+            ),
+            const Divider(),
+            ListTile(
+              leading: const Icon(Icons.logout, color: Colors.white),
+              title: const Text('Logout',
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                  textScaleFactor: 1.2),
+              onTap: () {
+                // Navigator.of(context).pushReplacementNamed(AuthScreen.routeName);
+                Provider.of<AuthProvider>(context, listen: false).logOut();
+                Navigator.pop(context);
+              },
+            )
+          ],
+        ),
       ),
     );
   }
